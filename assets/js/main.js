5670da1edfd2f9e145bd954a32919448d671e6ad
KingHasibul [HK] <83134617+kinghasibul@users.noreply.github.com>
2025-11-08 22:33:07 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,44 +22,7 @@ const linkAction = () => {
 
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-/*=============== CHANGE TEXT HOME SECTION ===============*/
-let words = document.querySelectorAll(".word");
-words.forEach((word)=>{
-   let letters = word.textContent.split("");
-   word.textContent="";
-   letters.forEach((letter)=>{
-      let span = document.createElement("span");
-      span.textContent = letter;
-      span.className = "letter";
-      word.append(span);
-   });
-});
 
-let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
-words[currentWordIndex].style.opacity = "1";
-
-let changeText = ()=>{
-   let currentWord = words[currentWordIndex];
-   let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-
-   Array.from(currentWord.children).forEach((letter,i)=>{
-      setTimeout(()=>{
-         letter.className = "letter out";
-      },i * 80);
-   });
-   nextWord.style.opacity="1";
-   Array.from(nextWord.children).forEach((letter,i)=>{
-      letter.className = "letter behind";
-      setTimeout(()=>{
-         letter.className = "letter in";
-      },340 + i * 80);
-   });
-   currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-};
-
-changeText();
-setInterval(changeText,3000);
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
@@ -69,65 +32,8 @@ const scrollHeader = () => {
 };
 
 window.addEventListener('scroll', scrollHeader);
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
-
-const scrollActive = () => {
-    const scrollY = window.pageYOffset;
-
-    sections.forEach((current) => {
-        const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 58,
-        sectionId = current.getAttribute('id'),
-        sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']');
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link');
-        } else {
-            sectionsClass.classList.remove('active-link');
-        }
-    });
-};
-
-window.addEventListener('scroll', scrollActive);
-/*=============== SERVICES SWIPER ===============*/
-var servicesSwiper = new Swiper(".services-swiper", {
-    spaceBetween: 32,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-        1208: {
-          slidesPerView: 3,
-        },
-    },
-});
-
-/*=============== Skills Section ===============*/
-const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=> {
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
-    var points ="";
-    var rotate = 360 / dots;
-
-    for(let i = 0 ; i < dots ; i++){
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-    };
-    elem.innerHTML = points;
 
 
-    const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i<percent ; i++) {
-        pointsMarked[i].classList.add('marked')
-    }
-});
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 var mixer = mixitup('.work-container', {
@@ -151,127 +57,6 @@ function activeWork() {
 
 linkWork.forEach((a) => a.addEventListener('click', activeWork));
 
-/*=============== RESUME ===============*/
-const accordionItems = document.querySelectorAll('.resume-item');
-accordionItems.forEach((item) => {
-    const header = item.querySelector('.resume-header'),
-    content = item.querySelector('.resume-content'),
-    icon = item.querySelector('.resume-icon i');
-
-    header.addEventListener('click', () => {
-        const isOpen = item.classList.toggle('accordion-open');
-
-        content.style.height = isOpen ? content.scrollHeight + 'px' : '0';
-        icon.className = isOpen ? 'ri-subtract-line' : 'ri-add-line';
-
-        accordionItems.forEach((otherItem) => {
-            if (otherItem !== item && otherItem.classList.contains('accordion-open')) {
-                otherItem.querySelector('.resume-content').style.height = '0';
-                otherItem.querySelector('.resume-icon i').classList= 'ri-add-line';
-                otherItem.classList.remove('accordion-open');
-            };
-        });
-    });
-});
-/*=============== TESTIMONIALS SWIPER ===============*/
-var servicesSwiper = new Swiper(".testimonials-swiper", {
-    spaceBetween: 32,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-        1208: {
-          slidesPerView: 3,
-        },
-    },
-});
-/*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById('contact-form'),
-contactName = document.getElementById('contact-name'),
-contactEmail = document.getElementById('contact-email'),
-contactSubject = document.getElementById('contact-subject'),
-contactMessage = document.getElementById('contact-message'),
-message = document.getElementById('message');
-
-const sendEmail = (e) => {
-    e.preventDefault();
-
-    if(contactName.value === '' || contactEmail.value === '' || contactSubject.value === '' || contactMessage.value === '') {
-        
-        message.classList.remove('color-first');
-        message.classList.add('color-red');
-        message.textContent = 'Write all the input fields';
-
-        setTimeout(() => {
-            message.textContent = '';
-        }, 3000);
-    } else {
-        emailjs.sendForm('service_v1lc7ia', 'template_e8vk1ep', '#contact-form', 'dXOg1EhQjMgidPYZ5').then(
-            () => {
-                message.classList.add('color-first');
-                message.textContent = 'Message sent ✔';
-
-                setTimeout(() => {
-                    message.textContent = '';
-                 }, 5000);
-            },
-             (error) => {
-                alert('OOPs! SOMETHING WENT WRONG...', error);
-            },
-        );
-        
-        contactName.value = '';
-        contactEmail.value = '';
-        contactSubject.value = '';
-        contactMessage.value = '';
-    };
-};
-
-contactForm.addEventListener('submit', sendEmail);
-
-/*=============== STYLE SWITCHER ===============*/
-
-const styleSwitcher = document.getElementById('style-switcher'),
-switcherToggle = document.getElementById('switcher-toggle'),
-switcherClose = document.getElementById('switcher-close');
-
-/* Switcher Show */ 
-switcherToggle.addEventListener('click', () => {
-    styleSwitcher.classList.add('show-switcher');
-});
-/* Switcher Hidden */
-switcherClose.addEventListener('click', () => {
-    styleSwitcher.classList.remove('show-switcher');
-});
-
-/*=============== THEME COLORS ===============*/
-const colors = document.querySelectorAll('.style-switcher-color');
-
-colors.forEach((color) => {
-    color.onclick = () => {
-        const activeColor = color.style.getPropertyValue('--hue');
-
-        colors.forEach((c) => c.classList.remove('active-color'));
-        color.classList.add('active-color');
-
-        document.documentElement.style.setProperty('--hue', activeColor);
-    };
-});
-/*=============== LIGHT/DARK MODE ===============*/
-let currentTheme = 'light';
-document.body.className = currentTheme;
-
-document.querySelectorAll('input[name="body-theme"]').forEach((input) => {
-    input.addEventListener('change', () => {
-        currentTheme = input.value;
-        document.body.className = currentTheme;
-    });
-});
 
 /*=============== SCROLL REVEAL ===============*/
 
